7bf8730900168fd6f76053bb73a16dd86b0b1b9f
SeaCoffee <sapienti8sat@gmail.com>
2023-02-14 13:53:23 +0200

    leSSon55

--- src/NewReducer.js
+++ src/NewReducer.js
@@ -4,32 +4,32 @@ function MyComponent() {
     const reducer = (state, action) =>  {
         switch (action.type) {
             case 'ADD':
-                return {...state, id: action.id, name: action.name}
+                return {...state, payload: action.payload}
 
             default:
                 return state
         }
     }
 
-            const action = {
-        type: 'add',
-        cat: {
-            name: ''
-
-        }
-    };
+        //     const action = {
+        // type: 'add',
+        // cat: {
+        //     name: ''
+        //
+        // }
+    // };
 
     const initState = () =>  ({cats: [], dogs:[]})
 
     const[state, dispatch] = useReducer(reducer, "", initState)
 
 
-    let cat = createRef()
+    let cats = createRef()
 
 
     const onSubmit = (e) => {
         e.preventDefault()
-        console.log(cat.current.value)
+        console.log(cats.current.value)
 
 
     }
@@ -42,11 +42,13 @@ function MyComponent() {
         <div>
             <form onSubmit={onSubmit}>
 
-           <input type="text" name={"Cat"} ref={cat}/>
+           <input type="text" name={"Cat"} ref={cats} onChange={(e) =>
+               dispatch({payload: cats.current.value})
+           }/>
             </form>
             <button>add cat</button>
             {/*<div>Cats: {state.cats}</div>*/}
-            {/*<button onClick={()=>dispatch({type:"CatsCount"})}>count</button>*/}
+            {/*<button onClick={()=>dispatch({payload: e.cat.current.value})}>count</button>*/}
         </div>
     );
 }
